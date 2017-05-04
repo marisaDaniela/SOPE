6c4d95361437ff2f1b056d926ed757ea32760a2f
marisaDaniela <up201308594@fe.up.pt>
2017-05-04 16:33:26 +0100

    start

---
+++ Projeto2/gerador.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <vector.h>
+
+typedef struct Person {
+	int id;
+	char gender;
+	double time;
+} person;
+
+
---
+++ Projeto2/sauna.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+	if(argc != 3){
+		printf("%s\n");
+
+	}
+	char* numRequest = argv[1];
+	char* maxUse = argv[2];
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
