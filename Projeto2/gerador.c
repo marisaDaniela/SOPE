61e441974f0ea89b063cfafb6fe8dcc6ecabb9bf
MellowDramaticPie <filipepintodosreis@gmail.com>
2017-05-08 16:31:56 +0100

    apaguei um dos main e cenas

--- Projeto2/gerador.c
+++ Projeto2/gerador.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <pthread.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
-char* INPUT_FIFO = "tmp/entrada";
-char* REJECTED_FIFO = "tmp/rejeitados";
+
 
 int ID = 1;
 
@@ -28,33 +28,29 @@ Person* generatePerson(int maxTime) {
 	person->time = rand() % maxTime + 1;
 	person->refusedTimes = 0;
 
+
 	return person;
 }
 
-int main(int argc, char** argv) {
-
-	if(argc != 3){
-		printf("Wrong number of arguments. Usage:<numRequests><maxTime>\n");
-	}
-
-	if(mkfifo(INPUT_FIFO, O_RDWR ) != 0){ // nao sei se vao ser estas as permissoes do fifo de entrada 
+void write_person(void * arg){
+					//meninas, ainda não sei fazer casts de variaveis, mas é preciso
+	if(mkfifo( (char *) arg, O_RDWR ) != 0){ // nao sei se vao ser estas as permissoes do fifo de entrada 
 		perror("Can't create FIFO 'tmp/entrada");
 		exit(1);
-	}
+		}	
+		
+        int fd; 
 
-	int numRequests = atoi(argv[1]); //numero de pedidos
-	int maxTime = atof(argv[2]); //max duracao de pedido
-	int fd; 
-
-	if(fd = open(INPUT_FIFO,O_WRONLY)==-1){
+	if(fd = open( (char *) arg, O_WRONLY)==-1){
 		perror("Oops!!");
 		exit(1);
-	}
+		}
 
 	Person * p = generatePerson(maxTime);
-
+		
 	write(fd, p, sizeof(p));
+	}
 
-	//INCOMPLETE
+	
+	
 
-}
--- Projeto2/sauna.c
+++ Projeto2/sauna.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include "gerador.c"
+
+
+char* INPUT_FIFO = "tmp/entrada";
+char* REJECTED_FIFO = "tmp/rejeitados";
+
 
 int main(int argc, char* argv[]){
-	if(argc != 2){
-		printf("Wrong number of arguments. Usage:<capacity>\n");
+
+	if(argc != 3){
+		printf("Wrong number of arguments. Usage:<numRequests><maxTime>\n");
 	}
 
-	int capacity = atoi(argv[1]);
+        int numRequests = atoi(argv[1]); //numero de pedidos
+	int maxTime = atof(argv[2]); //max duracao de pedido
+
+
+	pthread_t[numRequests] tid;
+
+
+	/*int 
+	while()*/
+	pthread_create(&tid[], NULL, write_person, char * INPUT_FIFO);
+	
+	
+	 
 }
